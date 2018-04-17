06d25ec7e2806465b4e2aea3b6153adcc4d9d73d
Luis Espinola <lespinola@gmail.com>
2018-04-17 08:39:22 -0400

    added event listener

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,30 @@
-//register service worker
-navigator.serviceWorker.register("../serviceworker.js");
-
 //event listeners.
 document.addEventListener("DOMContentLoaded", () => {
-  if (typeof fin != "undefined") {
-    fin.desktop.main(onMain);
-  } else {
-    ofVersion.innerText =
-      "OpenFin is not available - you are probably running in a browser.";
-  }
+	if (typeof fin != "undefined") {
+		fin.desktop.main(onMain);
+	} else {
+		ofVersion.innerText =
+			"OpenFin is not available - you are probably running in a browser.";
+	}
 });
 
 //Once the DOM has loaded and the OpenFin API is ready
 function onMain() {
-  const app = fin.desktop.Application.getCurrent();
-  fin.desktop.System.showDeveloperTools(app.uuid, app.uuid);
-  fin.desktop.System.getVersion(version => {
-    const ofVersion = document.querySelector("#of-version");
-    ofVersion.innerText = version;
-  });
+	const app = fin.desktop.Application.getCurrent();
+	fin.desktop.System.showDeveloperTools(app.uuid, app.uuid);
+	fin.desktop.System.getVersion(version => {
+		const ofVersion = document.querySelector("#of-version");
+		ofVersion.innerText = version;
+	});
+
+	window.addEventListener('contextmenu', onContextMenu, true);
+}
+
+function onContextMenu(event) {
+	if (event.ctrlKey && event.shiftKey && event.altKey) {
+		console.log('context menu opened...');
+		return;
+	}
+
+	event.preventDefault();
 }
